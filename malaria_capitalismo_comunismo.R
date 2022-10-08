ad1fe97c3de4807323936a9743e7ad568d9ff1ba
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-08 01:28:35 -0300

    Add files via upload

--- malaria_capitalismo_comunismo.R
+++ malaria_capitalismo_comunismo.R
@@ -57,5 +57,28 @@ malaria2 <- malaria %>%
 
 # Gráficos ---------------------------------------------------------------------------------------------------------------------------------
 
-  
-  
+c4a("safe", 6)
+
+ggplot(malaria1, aes(x = fct_reorder(Entity, media), 
+                     y = media, fill = Entity)) +
+  geom_col(width = 0.9) +
+  geom_errorbar(aes(ymin = media - se, ymax = media + se),
+                size = 0.8, width = 0.2) +
+  scale_fill_manual(values = c("#88CCEE", "#CC6677",
+                               "#DDCC77", "#117733",
+                               "#332288", "#AA4499")) +
+  scale_y_continuous(expand = expansion(mult = c(0,0))) +
+  labs(x = "Países", y = "Taxa de morte por malária") +
+  theme_ipsum(axis_title_size = 16, axis_text_size = 14) +
+  theme(legend.position = "none", axis.text = element_text(color = "black"))
+
+ggplot(malaria2, aes(x = Year, y = taxa_morte,
+                     group = Entity, color = Entity)) +
+  geom_point() +
+  geom_line(1.2) +
+  scale_color_manual(values = c("#88CCEE", "#CC6677",
+                               "#DDCC77", "#117733",
+                               "#332288", "#AA4499")) +
+  labs(x = "Tempo (anos)", y = "Taxa de morte por malária") +
+  theme_ipsum(axis_title_size = 16, axis_text_size = 14) +
+  theme(legend.position = "none", axis.text = element_text(color = "black"))
